0f068ccd1f9b0506ce7055745f14d056500cb3d0
tema <tema@vf8.ru>
2014-07-26 05:21:52 +0400

    File upload.

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Simple RESTful API
 
 * Node.js v0.10.29
 * npm install express (you need to have '/node_modules/express' in your work dir)
+* npm install multer (to handle uploads)
 * to POST and get response with curl: 'curl -i -X POST http://host:port/setherostats'
 * to GET JSON and picture just use our browser
+* to POST file with success: 'curl -i -F filedata=@superOk.jpg http://localhost:3000/uploadHeroImage'
+* to POST file with error (2Mb): 'curl -i -F filedata=@superBig.jpg http://localhost:3000/uploadHeroImage'
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+app.disable('x-powered-by'); // remove mess from header
 
-//app.configure(function () {
-//    app.use(express.methodOverride());
-//    app.use(express.bodyParser({keepExtensions: true, uploadDir: path.join(__dirname, '/files'}));
-//});
+// multer add upload support to express 4 (express 3 use connect instead)
+var multer = require('multer'); // to handle uploads
+app.use(multer({dest: './uploads/'}));
+
+var fs = require('fs'); // filesystem stuff (to move temp uploaded file)
 
 // set default superhero info
 var superhero = {};
@@ -24,7 +26,7 @@ app.get('/getHeroStats', function (req, res) {
 });
 
 app.get('/getHeroImage', function (req, res) {
-    res.sendfile('superhero.png');
+    res.sendfile('superhero.jpg');
 });
 
 app.post('/setHeroStats', function (req, res) {
@@ -32,11 +34,37 @@ app.post('/setHeroStats', function (req, res) {
     res.end('Done set params!');
 });
 
-// TODO: if no file then 'req.files' is undefined
 app.post('/uploadHeroImage', function (req, res) {
-    console.log(req.body);
+    res.setHeader("API Info", "This is info about file upload.");
     console.log(req.files);
-    res.end('Uploaded!');
+
+    // check if something uploaded and extension is JPG and size smaller than 1Mb
+    if (req.files && req.files.filedata.extension == 'jpg'
+        && req.files.filedata.size <= 1000000) {
+        console.log('JPG file received and it\' size smaller than 1Mb.');
+        var tmpPath = req.files.filedata.path;
+        var newPath = 'superhero.jpg';
+
+        fs.rename(tmpPath, newPath, function (err) {
+            if (err) {
+                res.json(err)
+            } else {
+                console.log('Uploaded file moved from temp dir to right place.');
+                res.json({uploadStatus: 'success'});
+            }
+        });
+    } else {
+        console.log('Some condition doesn\'t match. Maybe extension or filesize.');
+
+        // delete temp uploaded file
+        if (req.files) {
+            var tmpPath = req.files.filedata.path;
+            fs.unlink(tmpPath, function (err) {
+                if (err) throw err;
+            });
+        }
+        res.json({uploadStatus: 'error'});
+    }
 });
 
 
